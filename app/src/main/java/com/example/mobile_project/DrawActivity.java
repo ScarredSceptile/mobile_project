83e526b53d425684050d8599f9a39a9f20363415
ScarredSceptile <jonas.haegd@gmail.com>
2019-04-26 14:18:47 +0200

    Wordlist refined for better use, and we now get to know what word we have before we are able to begin drawing

--- app/src/main/java/com/example/mobile_project/DrawActivity.java
+++ app/src/main/java/com/example/mobile_project/DrawActivity.java
@@ -1,7 +1,12 @@
 package com.example.mobile_project;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Random;
+
 public class DrawActivity extends AppCompatActivity {
 
     MyCanvas myCanvas;
@@ -9,8 +14,27 @@ public class DrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_draw);
+
+        String[] words = this.getResources().getStringArray(R.array.words);
+        String drawWord = words[new Random().nextInt(words.length)];
+
+        final TextView wordView = findViewById(R.id.rndWord);
+        final TextView timerText = findViewById(R.id.textTimer);
         myCanvas = new MyCanvas(this,null);
-        setContentView(myCanvas);
+
+        wordView.setText(drawWord);
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timerText.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                setContentView(myCanvas);
+            }
+        }.start();
+
     }
 
 
