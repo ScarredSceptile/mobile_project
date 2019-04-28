a41d07b286c5a55f1fa691545c16b103f2c8cc92
ScarredSceptile <jonas.haegd@gmail.com>
2019-04-28 14:41:11 +0200

    Recombined Word and Draw activity

--- app/src/main/java/com/example/mobile_project/DrawActivity.java
+++ app/src/main/java/com/example/mobile_project/DrawActivity.java
@@ -4,34 +4,52 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Random;
 
 public class DrawActivity extends AppCompatActivity {
 
     MyCanvas myCanvas;
     private Bitmap b;
+    Button clearButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (ContextCompat.checkSelfPermission(DrawActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(DrawActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }
 
-        setContentView(R.layout.activity_draw);
-        Button clearButton = findViewById(R.id.button2);
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
+        setContentView(R.layout.activity_word);
+
+        String[] words = this.getResources().getStringArray(R.array.words);
+        String drawWord = words[new Random().nextInt(words.length)];
+
+        final TextView wordView = findViewById(R.id.rndWord);
+        final TextView timerText = findViewById(R.id.textTimer);
+
+        wordView.setText(drawWord);
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timerText.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                setContentView(R.layout.activity_draw);
+
+                clearButton = findViewById(R.id.button2);
+
+                clearButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
                 try{
                     myCanvas.setDrawingCacheEnabled(true);
                     b = myCanvas.getDrawingCache();
@@ -63,40 +81,17 @@ public class DrawActivity extends AppCompatActivity {
                 }
 
 
-            }
+                    }
 
-        });
+                });
+
+            }
+        }.start();
+
+
 
 
 
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(DrawActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-
-    }
-
 
 }
--- app/src/main/java/com/example/mobile_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_project/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.mobile_project;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
 
         Button drawButton = findViewById(R.id.button);
         drawButton.setOnClickListener(new View.OnClickListener() {
@@ -21,14 +32,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(draw);
             }
         });
-        Button wordButton = findViewById(R.id.button5);
-        wordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                final Intent word = new Intent(MainActivity.this, WordActivity.class);
-                startActivity(word);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
             }
-        });
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
     }
 
 }
